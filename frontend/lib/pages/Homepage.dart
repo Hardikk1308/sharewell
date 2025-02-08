953f803bb16796f425f3fdf63ae5a2adcd7b44ee
AbhishekSinghDIkhit <aabhishek576@gmail.com>
2025-02-08 18:00:08 +0530

    updated

--- frontend/lib/pages/Homepage.dart
+++ frontend/lib/pages/Homepage.dart
@@ -26,7 +26,7 @@ class _HomepageState extends State<Homepage> {
   bool _isLoading = false;
   final ImagePicker _picker = ImagePicker();
 
-  final String _apiUrl = " https://33dc-112-196-126-3.ngrok-free.app/analyze-food/"; // Change this to your FastAPI URL
+  final String _apiUrl = "https://33dc-112-196-126-3.ngrok-free.app/analyze-food/"; // Change this to your FastAPI URL
 
   @override
   void initState() {
