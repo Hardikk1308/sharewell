93f2ddb536e52c205e0202b7a28ec0ada83432a9
hardik pawar <hardikpawar2811@gmail.com>
2025-02-08 21:49:51 +0530

    updated commit

--- frontend/lib/Volunteersform/Avability.dart
+++ frontend/lib/Volunteersform/Avability.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../common/basic_app_buttons.dart';
+import '../constant/App_Colour.dart';
 import '../pages/Volunteers_homepage.dart';
 
 class AvailabilityPage extends StatefulWidget {
@@ -54,23 +56,35 @@ class _AvailabilityPageState extends State<AvailabilityPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text("Select your availability")),
+        backgroundColor: AppColors.background,
+        appBar: AppBar(
+          title: Text(
+            "Select your availability",
+            style: GoogleFonts.bricolageGrotesque(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: AppColors.background,
+          automaticallyImplyLeading: false,
+        ),
         body: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  "Select your availability",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Text(
+                    "Select your availability",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
-                SizedBox(height: 8),
-                Text(
-                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry"),
-                SizedBox(height: 16),
 
-                // Time Slot Selection
                 Column(
                   children: _timeSlots.map((slot) {
                     return RadioListTile(
@@ -85,52 +99,49 @@ class _AvailabilityPageState extends State<AvailabilityPage> {
                     );
                   }).toList(),
                 ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    onPressed: () {
-                      // Implement manual time picker
-                    },
-                    child: Text("Set time manually"),
-                  ),
-                ),
-                SizedBox(height: 16),
+
+                SizedBox(height: 15),
 
                 // Day & Date Selection
-                Text("Day & Date",
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                SizedBox(height: 8),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Text("Day & Date",
+                      style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      )),
+                ),
+                SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: ["S", "M", "T", "W", "T", "F", "S"]
-                      .asMap()
-                      .entries
-                      .map((entry) {
-                    int index = entry.key;
-                    String day = entry.value;
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedDays[index] = !_selectedDays[index];
-                        });
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: _selectedDays[index]
-                              ? Colors.blue
-                              : Colors.grey[300],
-                          borderRadius: BorderRadius.circular(5),
+                  children:
+                      ["S", "M", "T", "W", "T", "F", "S"].asMap().entries.map(
+                    (entry) {
+                      int index = entry.key;
+                      String day = entry.value;
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedDays[index] = !_selectedDays[index];
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: _selectedDays[index]
+                                ? Colors.blue
+                                : Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(day,
+                              style: TextStyle(
+                                  color: _selectedDays[index]
+                                      ? Colors.white
+                                      : Colors.black)),
                         ),
-                        child: Text(day,
-                            style: TextStyle(
-                                color: _selectedDays[index]
-                                    ? Colors.white
-                                    : Colors.black)),
-                      ),
-                    );
-                  }).toList(),
+                      );
+                    },
+                  ).toList(),
                 ),
                 SizedBox(height: 10),
 
@@ -145,6 +156,12 @@ class _AvailabilityPageState extends State<AvailabilityPage> {
                     });
                   },
                 ),
+                Divider(
+                  color: Colors.grey,
+                  thickness: 1,
+                  indent: 2,
+                  endIndent: 2,
+                ),
                 CheckboxListTile(
                   title: Text("All Weekend"),
                   value: _allWeekend,
@@ -155,6 +172,12 @@ class _AvailabilityPageState extends State<AvailabilityPage> {
                       _anyDay = false;
                     });
                   },
+                ),
+                Divider(
+                  color: Colors.grey,
+                  thickness: 1,
+                  indent: 2,
+                  endIndent: 2,
                 ),
                 CheckboxListTile(
                   title: Text("Any Day"),
@@ -183,6 +206,7 @@ class _AvailabilityPageState extends State<AvailabilityPage> {
                     text: 'Continue',
                   ),
                 ),
+                SizedBox(height: 30),
               ],
             ),
           ),
